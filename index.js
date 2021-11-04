49f0d671082a0738b570ac73518806a8cc64b9c0
Sean Martin <sean@seanmartinstudio.com>
2021-11-04 11:42:27 -0700

    commit tree 851443971099642b3e06f84fadbeaef66f02eeff

--- index.js
+++ index.js
@@ -1,2 +1,20 @@
+//
+//multiply; 
+const num1 = 31;
+const num2 = 2;
+const multiply = num1 * num2;
+console.log(multiply);
 
-// Write your code here
+//random
+let random = Math.floor(Math.random() * 10) + 1;
+console.log(random);
+
+//mod
+const num3 = 10;
+const num4 = 6;
+const mod = num3 % num4;
+console.log(mod);
+
+//max
+const max = Math.max(19,20);
+console.log(max);
